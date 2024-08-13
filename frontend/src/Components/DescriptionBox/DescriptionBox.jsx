a6adc88aded22144240ae0e876eb7bd622ff80c0
keer324 <keerthimoger@gmail.com>
2024-08-13 16:25:18 +0530

    product page modified

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+    return (
+        <div className='descriptionbox'>
+            <div className="decriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122) </div>
+            </div>
+            <div className="descriptionbx-description">
+                <p>An e-commerce website is an online
+                    platform that facilitate the buying and selling of
+                    products or services over the internet. serves as a virtual
+                    marketplace Lorem ipsum dolor sit amet consectetur adipisicing elit
+                    Laboriosam error deserunt, rem sint odit repellat sunt eveniet culpa in omnis.</p>
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox
--- frontend/src/Pages/Products.jsx
+++ frontend/src/Pages/Products.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom';
+import BreadCrumb from '../Components/BreadCrumbs/BreadCrumb';
+import ProductDisplay from '../Components/Product_Display/ProductDisplay';
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
 
 const Products = () => {
+    const { all_product } = useContext(ShopContext);
+    const { productId } = useParams();
+    const product = all_product.find((e) => e.id === Number(productId));
     return (
-        <div>Products</div>
+        <div>
+            <BreadCrumb product={product} />
+            <ProductDisplay product={product} />
+            <DescriptionBox />
+        </div>
+
     )
 }
 
